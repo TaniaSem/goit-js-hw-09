3e874e434d0c8b1ce552fb0232f30804622b9c89
TaniaSem <semenchuktetiana@gmail.com>
2022-08-14 18:14:30 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.body,
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+refs.stopBtn.disabled = true;
+
+let intervalId = 0;
+
+function onStartBtnClick(e) {
+  e.target.disabled = true;
+  refs.stopBtn.disabled = false;
+  intervalId = setInterval(colorSwitcher, 1000);
+}
+
+function onStopBtnClick(e) {
+  clearInterval(intervalId);
+  e.target.disabled = true;
+  refs.startBtn.disabled = false;
+}
+
+function colorSwitcher() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
