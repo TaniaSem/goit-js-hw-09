4c88deb198a9e39b9eefcc4919ac43ecd96588eb
TaniaSem <semenchuktetiana@gmail.com>
2022-08-15 00:15:15 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const elements = e.target.elements;
+  let delay = Number(elements.delay.value);
+  const step = Number(elements.step.value);
+  const amount = Number(elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
